36197ed3d7d9bca2b81f773163a924df94f428b6
Greg Rodriguez <greg_orio15323@yahoo.com>
2020-10-11 00:09:17 -0600

    updates

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ var questions = [{
     answer:
 },
 {
-    title:,
-    choices:,
-    answer:
+    title: "Which of the following function of String object combines the text of two strings and returns a new string?",
+    choices: ["add( )", "concat( )", " merge( )", "append( )"],
+    answer: "concat( )"
 },
 {
     title:,
@@ -76,3 +76,71 @@ var quizContent = `
     
 document.getElementById("quizBody").innerHTML = quizContent;
 }
+
+function clearScore() {
+    localStorage.setItem("highscore", "");
+    localStorage.setItem("highscoreName",  "");
+    
+    resetGame();
+    };
+
+function resetGame() {
+    clearInterval(timer);
+    score = 0;
+    currentQuestion = -1;
+    timeLeft = 0;
+    timer = null;
+        
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+    
+var quizContent = `
+<h1>
+ Coding Quiz Challenge!
+ </h1>
+ <h4>
+  Try to answer correctly and get the high score!
+ </h4>
+ <h3>
+  Click to play!
+ </h3>
+ <button onclick="start()">Start!</button>`;
+
+ document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+function incorrect() {
+    timeLeft -= 10; 
+    next();
+}
+
+function correct() {
+    score += 20;
+    next();
+}
+
+function next() {
+    currentQuestion++;
+    
+    if (currentQuestion > questions.length - 1) {
+        endGame();
+        return;
+}
+    
+    var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
+    
+    for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {
+        var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>"; 
+        buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].choices[buttonLoop]);
+        if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {
+            buttonCode = buttonCode.replace("[ANS]", "correct()");
+        } else {
+            buttonCode = buttonCode.replace("[ANS]", "incorrect()");
+        }
+        quizContent += buttonCode
+}
+    
+    
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+    
